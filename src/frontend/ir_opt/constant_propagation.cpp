49df3a7ef598cb8714c9a5b08117b3c3ac31132b
fleroviux <fleroviux@gmail.com>
2022-09-04 13:38:48 +0200

    IROpt: cleanup constant propagation pass

--- src/frontend/ir_opt/constant_propagation.cpp
+++ src/frontend/ir_opt/constant_propagation.cpp
@@ -5,241 +5,237 @@
  * found in the LICENSE file.
  */
 
-#include <vector>
-
 #include "common/bit.hpp"
 #include "frontend/ir_opt/constant_propagation.hpp"
 
 namespace lunatic {
 namespace frontend {
 
+const auto p = [](std::unique_ptr<IROpcode> const& op) {
+  fmt::print("propagate: {}\n", op->ToString());
+};
+
 void IRConstantPropagationPass::Run(IREmitter& emitter) {
-  const auto p = [&](std::unique_ptr<IROpcode> const& op) {
-    fmt::print("propagate: {}\n", op->ToString());
-  };
-
-  std::vector<Optional<IRConstant>> var_to_const{};
-
+  this->emitter = &emitter;
+  var_to_const.clear();
   var_to_const.resize(emitter.Vars().size());
 
-  const auto Propagate = [&](IRVariable const& var, IRConstant const& constant) {
-    var_to_const[var.id] = constant;
-
-    // TODO: start at the opcode where the variable is first written.
-    for (auto& op : emitter.Code()) {
-      if (op->Reads(var)) {
-        fmt::print("propagated to: {}\n", op->ToString());
-        op->PropagateConstant(var, constant);
-      }
-    }
-  };
-
-  const auto GetKnownConstant = [&](IRVarRef const& var)-> Optional<IRConstant>& {
-    return var_to_const[var.Get().id];
-  };
-
-  const auto MOV = [](IRVariable const& result, IRConstant const& constant, bool update_host_flags = false) {
-    return std::make_unique<IRMov>(result, constant, update_host_flags);
-  };
-
-  const auto NOP = []() {
-    return std::make_unique<IRNoOp>();
-  };
-
   for (auto& op : emitter.Code()) {
-    const auto op_class = op->GetClass();
-
-    switch (op_class) {
-      case IROpcodeClass::MOV: {
-        const auto mov = (IRMov*)op.get();
-
-        if (mov->source.IsConstant()) {
-          p(op);
-          Propagate(mov->result.Get(), mov->source.GetConst());
-        }
-        break;
-      }
-      case IROpcodeClass::LSL: {
-        const auto lsl = (IRLogicalShiftLeft*)op.get();
-
-        auto& result = lsl->result.Get();
-        auto& operand = GetKnownConstant(lsl->operand);
-        auto& amount = lsl->amount;
-
-        if (operand.HasValue() && amount.IsConstant()) {
-          int shift_amount = amount.GetConst().value & 255;
-          IRConstant constant = shift_amount >= 32 ? 0 : (operand.Unwrap().value << shift_amount);
-
-          p(op);
-          Propagate(result, constant);
-
-          if (!lsl->update_host_flags) {
-            op = MOV(result, constant);
-          }
-        }
-        break;
-      }
-      case IROpcodeClass::LSR: {
-        const auto lsr = (IRLogicalShiftRight*)op.get();
-
-        auto& result = lsr->result.Get();
-        auto& operand = GetKnownConstant(lsr->operand);
-        auto& amount = lsr->amount;
-
-        if (operand.HasValue() && amount.IsConstant()) {
-          int shift_amount = amount.GetConst().value & 255;
-          IRConstant constant = (shift_amount == 0 || shift_amount >= 32) ? 0 : (operand.Unwrap().value >> shift_amount);
-
-          p(op);
-          Propagate(result, constant);
-
-          if (!lsr->update_host_flags) {
-            op = MOV(result, constant);
-          }
-        }
-        break;
-      }
-      case IROpcodeClass::ASR: {
-        const auto asr = (IRArithmeticShiftRight*)op.get();
-
-        auto& result = asr->result.Get();
-        auto& operand = GetKnownConstant(asr->operand);
-        auto& amount = asr->amount;
-
-        if (operand.HasValue() && amount.IsConstant()) {
-          int shift_amount = amount.GetConst().value & 255;
-          u32 operand_value = operand.Unwrap().value;
-
-          if (shift_amount == 0 || shift_amount >= 32) {
-            shift_amount = 31;
-          }
-
-          IRConstant constant = (u32)((s32)operand_value >> shift_amount);
-
-          p(op);
-          Propagate(result, constant);
-
-          if (!asr->update_host_flags) {
-            op = MOV(result, constant);
-          }
-        }
-        break;
-      }
-      case IROpcodeClass::ROR: {
-        const auto ror = (IRRotateRight*)op.get();
-
-        auto& result = ror->result.Get();
-        auto& operand = GetKnownConstant(ror->operand);
-        auto& amount = ror->amount;
-
-        if (operand.HasValue() && amount.IsConstant()) {
-          int shift_amount = amount.GetConst().value;
-
-          if (shift_amount == 0) {
-            // RRX #1
-            break;
-          }
-
-          IRConstant constant = bit::rotate_right(operand.Unwrap().value, shift_amount & 31);
-
-          p(op);
-          Propagate(ror->result.Get(), constant);
-
-          if (!ror->update_host_flags) {
-            op = MOV(result, constant);
-          }
-        }
-        break;
-      }
-      case IROpcodeClass::ADD:
-      case IROpcodeClass::SUB:
-      case IROpcodeClass::AND:
-      case IROpcodeClass::BIC:
-      case IROpcodeClass::EOR:
-      case IROpcodeClass::ORR: {
-        const auto add = (IRAdd*)op.get(); // TODO: not always correct
-
-        auto& result = add->result;
-        auto& lhs = GetKnownConstant(add->lhs);
-        auto& rhs = add->rhs;
-
-        if (lhs.HasValue() && rhs.IsConstant()) {
-          IRConstant constant;
-
-          u32 lhs_value = lhs.Unwrap().value;
-          u32 rhs_value = rhs.GetConst().value;
-
-          switch (op_class) {
-            case IROpcodeClass::ADD: constant = lhs_value +  rhs_value; break;
-            case IROpcodeClass::SUB: constant = lhs_value -  rhs_value; break;
-            case IROpcodeClass::AND: constant = lhs_value &  rhs_value; break;
-            case IROpcodeClass::BIC: constant = lhs_value & ~rhs_value; break;
-            case IROpcodeClass::EOR: constant = lhs_value ^  rhs_value; break;
-            case IROpcodeClass::ORR: constant = lhs_value |  rhs_value; break;
-          }
-
-          if (result.HasValue()) {
-            p(op);
-            Propagate(result.Unwrap(), constant);
-          }
-
-          bool update_host_flags = add->update_host_flags;
-
-          // Attempt to replace opcode with a MOV (removes dependencies on operand variables)
-          if (result.HasValue()) {
-            if (op_class == IROpcodeClass::ADD || op_class == IROpcodeClass::SUB) {
-              if (!update_host_flags) {
-                op = MOV(result.Unwrap(), constant, false);
-              }
-            } else {
-              // AND, BIC, EOR, ORR
-              op = MOV(result.Unwrap(), constant, update_host_flags);
-            }
-          } else if (!update_host_flags) {
-            op = NOP();
-          }
-        }
-
-        break;
-      }
-      case IROpcodeClass::MUL: {
-        const auto mul = (IRMultiply*)op.get();
-
-        auto& lhs = GetKnownConstant(mul->lhs.Get());
-        auto& rhs = GetKnownConstant(mul->rhs.Get());
-
-        if (lhs.HasValue() && rhs.HasValue()) {
-          if (mul->result_hi.HasValue()) {
-            if (mul->lhs.Get().data_type == IRDataType::SInt32) {
-              s64 result = (s64)(s32)lhs.Unwrap().value * (s64)(s32)rhs.Unwrap().value;
-              IRConstant constant_lo = (u32)result;
-              IRConstant constant_hi = (u32)(result >> 32);
-
-              p(op);
-              Propagate(mul->result_lo.Get(), constant_lo);
-              Propagate(mul->result_hi.Unwrap(), constant_hi);
-            } else {
-              u64 result = (u64)lhs.Unwrap().value * (u64)rhs.Unwrap().value;
-              IRConstant constant_lo = (u32)result;
-              IRConstant constant_hi = (u32)(result >> 32);
-
-              p(op);
-              Propagate(mul->result_lo.Get(), constant_lo);
-              Propagate(mul->result_hi.Unwrap(), constant_hi);
-            }
-          } else {
-            IRConstant constant = lhs.Unwrap().value * rhs.Unwrap().value;
-
-            p(op);
-            Propagate(mul->result_lo.Get(), constant);
-            op = MOV(mul->result_lo.Get(), constant, mul->update_host_flags);
-          }
-        }
-        break;
-      }
+    switch (op->GetClass()) {
+      case IROpcodeClass::MOV: DoMOV(op); break;
+      case IROpcodeClass::LSL: DoLSL(op); break;
+      case IROpcodeClass::LSR: DoLSR(op); break;
+      case IROpcodeClass::ASR: DoASR(op); break;
+      case IROpcodeClass::ROR: DoROR(op); break;
+      case IROpcodeClass::ADD: DoBinaryOp<IRAdd>(op); break;
+      case IROpcodeClass::SUB: DoBinaryOp<IRSub>(op); break;
+      case IROpcodeClass::AND: DoBinaryOp<IRBitwiseAND>(op); break;
+      case IROpcodeClass::BIC: DoBinaryOp<IRBitwiseBIC>(op); break;
+      case IROpcodeClass::EOR: DoBinaryOp<IRBitwiseEOR>(op); break;
+      case IROpcodeClass::ORR: DoBinaryOp<IRBitwiseORR>(op); break;
+      case IROpcodeClass::MUL: DoMUL(op); break;
     }
   }
 
+}
+
+void IRConstantPropagationPass::Propagate(IRVariable const& var, IRConstant const& constant) {
+  var_to_const[var.id] = constant;
+
+  // TODO: start at the opcode where the variable is first written.
+  for (auto& op : emitter->Code()) {
+    if (op->Reads(var)) {
+      fmt::print("propagated to: {}\n", op->ToString());
+      op->PropagateConstant(var, constant);
+    }
+  }
+};
+
+auto IRConstantPropagationPass::GetKnownConstant(IRVarRef const& var) -> Optional<IRConstant>& {
+  return var_to_const[var.Get().id];
+};
+
+void IRConstantPropagationPass::DoMOV(std::unique_ptr<IROpcode>& op) {
+  auto mov_op = lunatic_cast<IRMov>(op.get());
+
+  if (mov_op->source.IsConstant()) {
+    p(op);
+    Propagate(mov_op->result.Get(), mov_op->source.GetConst());
+  }
+}
+
+void IRConstantPropagationPass::DoLSL(std::unique_ptr<IROpcode>& op) {
+  auto lsl_op = lunatic_cast<IRLogicalShiftLeft>(op.get());
+
+  auto& result = lsl_op->result.Get();
+  auto& operand = GetKnownConstant(lsl_op->operand);
+  auto& amount = lsl_op->amount;
+
+  if (operand.HasValue() && amount.IsConstant()) {
+    int shift_amount = (int)(amount.GetConst().value & 255);
+    IRConstant constant = shift_amount >= 32 ? 0 : (operand.Unwrap().value << shift_amount);
+
+    p(op);
+    Propagate(result, constant);
+
+    if (!lsl_op->update_host_flags) {
+      op = MOV(result, constant);
+    }
+  }
+}
+
+void IRConstantPropagationPass::DoLSR(std::unique_ptr<IROpcode>& op) {
+  auto lsr_op = lunatic_cast<IRLogicalShiftRight>(op.get());
+
+  auto& result = lsr_op->result.Get();
+  auto& operand = GetKnownConstant(lsr_op->operand);
+  auto& amount = lsr_op->amount;
+
+  if (operand.HasValue() && amount.IsConstant()) {
+    int shift_amount = (int)(amount.GetConst().value & 255);
+    IRConstant constant = (shift_amount == 0 || shift_amount >= 32) ? 0 : (operand.Unwrap().value >> shift_amount);
+
+    p(op);
+    Propagate(result, constant);
+
+    if (!lsr_op->update_host_flags) {
+      op = MOV(result, constant);
+    }
+  }
+}
+
+void IRConstantPropagationPass::DoASR(std::unique_ptr<IROpcode>& op) {
+  auto asr_op = lunatic_cast<IRArithmeticShiftRight>(op.get());
+
+  auto& result = asr_op->result.Get();
+  auto& operand = GetKnownConstant(asr_op->operand);
+  auto& amount = asr_op->amount;
+
+  if (operand.HasValue() && amount.IsConstant()) {
+    int shift_amount = (int)(amount.GetConst().value & 255);
+    u32 operand_value = operand.Unwrap().value;
+
+    if (shift_amount == 0 || shift_amount >= 32) {
+      shift_amount = 31;
+    }
+
+    IRConstant constant = (u32)((s32)operand_value >> shift_amount);
+
+    p(op);
+    Propagate(result, constant);
+
+    if (!asr_op->update_host_flags) {
+      op = MOV(result, constant);
+    }
+  }
+}
+
+void IRConstantPropagationPass::DoROR(std::unique_ptr<IROpcode>& op) {
+  auto ror_op = lunatic_cast<IRRotateRight>(op.get());
+
+  auto& result = ror_op->result.Get();
+  auto& operand = GetKnownConstant(ror_op->operand);
+  auto& amount = ror_op->amount;
+
+  if (operand.HasValue() && amount.IsConstant()) {
+    u32 shift_amount = amount.GetConst().value;
+
+    if (shift_amount == 0) {
+      // RRX #1
+      return;
+    }
+
+    IRConstant constant = bit::rotate_right(operand.Unwrap().value, shift_amount & 31);
+
+    p(op);
+    Propagate(ror_op->result.Get(), constant);
+
+    if (!ror_op->update_host_flags) {
+      op = MOV(result, constant);
+    }
+  }
+}
+
+template<typename OpcodeType>
+void IRConstantPropagationPass::DoBinaryOp(std::unique_ptr<IROpcode>& op) {
+  constexpr IROpcodeClass klass = OpcodeType::klass;
+
+  auto bin_op = lunatic_cast<OpcodeType>(op.get());
+  auto& result = bin_op->result;
+  auto& lhs = GetKnownConstant(bin_op->lhs);
+  auto& rhs = bin_op->rhs;
+
+  if (lhs.HasValue() && rhs.IsConstant()) {
+    IRConstant constant;
+
+    u32 lhs_value = lhs.Unwrap().value;
+    u32 rhs_value = rhs.GetConst().value;
+
+    switch (klass) {
+      case IROpcodeClass::ADD: constant = lhs_value +  rhs_value; break;
+      case IROpcodeClass::SUB: constant = lhs_value -  rhs_value; break;
+      case IROpcodeClass::AND: constant = lhs_value &  rhs_value; break;
+      case IROpcodeClass::BIC: constant = lhs_value & ~rhs_value; break;
+      case IROpcodeClass::EOR: constant = lhs_value ^  rhs_value; break;
+      case IROpcodeClass::ORR: constant = lhs_value |  rhs_value; break;
+    }
+
+    if (result.HasValue()) {
+      p(op);
+      Propagate(result.Unwrap(), constant);
+    }
+
+    bool update_host_flags = bin_op->update_host_flags;
+
+    // Attempt to replace opcode with a MOV (removes dependencies on operand variables)
+    if (result.HasValue()) {
+      if (klass == IROpcodeClass::ADD || klass == IROpcodeClass::SUB) {
+        if (!update_host_flags) {
+          op = MOV(result.Unwrap(), constant, false);
+        }
+      } else {
+        // AND, BIC, EOR, ORR
+        op = MOV(result.Unwrap(), constant, update_host_flags);
+      }
+    } else if (!update_host_flags) {
+      op = NOP();
+    }
+  }
+}
+
+void IRConstantPropagationPass::DoMUL(std::unique_ptr<IROpcode>& op) {
+  const auto mul_op = (IRMultiply*)op.get();
+
+  auto& lhs = GetKnownConstant(mul_op->lhs.Get());
+  auto& rhs = GetKnownConstant(mul_op->rhs.Get());
+
+  if (lhs.HasValue() && rhs.HasValue()) {
+    if (mul_op->result_hi.HasValue()) {
+      if (mul_op->lhs.Get().data_type == IRDataType::SInt32) {
+        s64 result = (s64)(s32)lhs.Unwrap().value * (s64)(s32)rhs.Unwrap().value;
+        IRConstant constant_lo = (u32)result;
+        IRConstant constant_hi = (u32)(result >> 32);
+
+        p(op);
+        Propagate(mul_op->result_lo.Get(), constant_lo);
+        Propagate(mul_op->result_hi.Unwrap(), constant_hi);
+      } else {
+        u64 result = (u64)lhs.Unwrap().value * (u64)rhs.Unwrap().value;
+        IRConstant constant_lo = (u32)result;
+        IRConstant constant_hi = (u32)(result >> 32);
+
+        p(op);
+        Propagate(mul_op->result_lo.Get(), constant_lo);
+        Propagate(mul_op->result_hi.Unwrap(), constant_hi);
+      }
+    } else {
+      IRConstant constant = lhs.Unwrap().value * rhs.Unwrap().value;
+
+      p(op);
+      Propagate(mul_op->result_lo.Get(), constant);
+      op = MOV(mul_op->result_lo.Get(), constant, mul_op->update_host_flags);
+    }
+  }
 }
 
 } // namespace lunatic::frontend
--- src/frontend/ir_opt/constant_propagation.hpp
+++ src/frontend/ir_opt/constant_propagation.hpp
@@ -7,6 +7,8 @@
 
 #pragma once
 
+#include <vector>
+
 #include "frontend/ir_opt/pass.hpp"
 
 namespace lunatic {
@@ -14,6 +16,31 @@ namespace frontend {
 
 struct IRConstantPropagationPass final : IRPass {
   void Run(IREmitter& emitter) override;
+
+private:
+  void Propagate(IRVariable const& var, IRConstant const& constant);
+  auto GetKnownConstant(IRVarRef const& var) -> Optional<IRConstant>&;
+
+  void DoMOV(std::unique_ptr<IROpcode>& op);
+  void DoLSL(std::unique_ptr<IROpcode>& op);
+  void DoLSR(std::unique_ptr<IROpcode>& op);
+  void DoASR(std::unique_ptr<IROpcode>& op);
+  void DoROR(std::unique_ptr<IROpcode>& op);
+  void DoMUL(std::unique_ptr<IROpcode>& op);
+
+  template<typename OpcodeType>
+  void DoBinaryOp(std::unique_ptr<IROpcode>& op);
+
+  static auto MOV(IRVariable const& result, IRConstant const& constant, bool update_host_flags = false) {
+    return std::make_unique<IRMov>(result, constant, update_host_flags);
+  };
+
+  static auto NOP() {
+    return std::make_unique<IRNoOp>();
+  };
+
+  IREmitter* emitter;
+  std::vector<Optional<IRConstant>> var_to_const{};
 };
 
 } // namespace lunatic::frontend
